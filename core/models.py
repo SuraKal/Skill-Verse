6fa8a632b619912085eda632483793411debb785
Surafel Kaleab <surafelkaleab68@gmail.com>
2026-03-31 05:40:16 -0700

    profile setup done- added bio functionality

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    uuid = models.CharField(max_length=255, unique=True, null=True)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+    def __str__(self) -> str:
+        return self.user.username
+

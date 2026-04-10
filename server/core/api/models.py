e197802e14b1c1396acddd6af84ca0c06f876f61
Surafel Kaleab <surafelkaleab68@gmail.com>
2026-04-10 11:00:55 -0700

    Added notification

--- server/core/api/models.py
+++ server/core/api/models.py
@@ -339,3 +339,14 @@ class Message(models.Model):
 
     def __str__(self):
         return f"Message from {self.sender.username} to {self.recipient.username}"
+    
+class Notification(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="notifications")
+    content = models.TextField()
+    is_read = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Notification for {self.user.username} - {self.content[:50]}..."

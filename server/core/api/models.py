6234dcbf4cbe5eca0d318350688dfab025f20e50
Surafel Kaleab <surafelkaleab68@gmail.com>
2026-04-10 10:58:22 -0700

    Added announcement

--- server/core/api/models.py
+++ server/core/api/models.py
@@ -302,3 +302,18 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"{self.user.username} commented on {self.course.title}"
+
+class Announcement(models.Model):
+    STATUS_CHOICES = [
+        ('draft', 'Draft'),
+        ('published', 'Published')
+    ]
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    status = models.CharField(
+        max_length=20, choices=STATUS_CHOICES, default='draft')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.course.title} - {self.title}"
+

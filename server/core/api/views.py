c0bd2a113e3d987670c4ebb2e5cc3a9d82a5b688
Surafel Kaleab <surafelkaleab68@gmail.com>
2026-04-14 02:51:49 -0700

    before adding admin UI

--- server/core/api/views.py
+++ server/core/api/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Course, Assessment, Enrollment, Certificate, Review, Comment
+from .serializer import CourseSerializer, AssessmentViewSet, EnrollmentViewSet, CertificateViewSet, ReviewViewSet, CommentViewSet  
+from .pagination import DefaultPagination
+from rest_framework import viewsets
+
+
 
 # Create your views here.
+class CourseViewSet(viewsets.ModelViewSet):
+    queryset = Course.objects.all().order_by('-id')
+    serializer_class = CourseSerializer
+    pagination_class = DefaultPagination
+
+
+
+
